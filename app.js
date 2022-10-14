829463d2501ebfe29c9141e6095a6a4fe3f6903c
Evy <evetricia.k@hotmail.fr>
2022-10-15 00:26:54 +0200

    section studies ajoutée, section portfolio en cours, des modifications faites sur chacune des parties. ensemble à paufiner

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const hamburgerBtn = document.getElementById("hambuger__button");
 const line = document.querySelectorAll(".line");
 const nav = document.querySelector(".nav");
-const animTitleSpan = document.querySelector(".accueil__title-span");
+const animTitle = document.querySelector(".accueil__title");
 
 
 // Permet l'ajout de class et l'ouverture et l'animation des différents éléments.
@@ -18,39 +18,41 @@ addClasslitActive(hamburgerBtn, nav);
 
 
 //typewritter
-new Typewriter(animTitleSpan, {
-  // loop: true,
+new Typewriter(animTitle, {
+  loop: true,
   deleteSpeed: 20
 })
   .changeDelay(20)
+  .typeString('Développeuse<br>web junior')
   .typeString(' <strong>HTML <span style="color: #f4c44e"><i class="fa-brands fa-html5"></i></span</strong>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(7)
   .typeString(' <strong>CSS <span style="color: #0068ba"><i class="fa-brands fa-css3-alt"></i></span</strong>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(6)
   .typeString(' <strong>SASS <span style="color: #c76395"><i class="fa-brands fa-sass"></i></span</strong>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(7)
   .typeString(' <strong>Bootstrap <span style="color: #6f10ee"><i class="fa-brands fa-bootstrap"></i></span</strong>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(11)
   .typeString(' <strong>JavaScript <span style="color: #f4c44e"><i class="fa-brands fa-square-js"></i></span</strong>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(12)
   .typeString('<strong><span style="color: #3e4035">Node</span><span style="color: #6ab849"><i class="fa-brands fa-node-js"></i></span></strong>')
-  .pauseFor(1000)
-  .deleteChars(9)
+  .pauseFor(300)
+  .deleteChars(7)
   .typeString('<strong style="color: #3e4035">Express </strong><span style= "color: #dd8a00"><i class="fa-solid fa-gears"></i></span>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(9)
   .typeString('<strong>MongoDB <span style= "color: #27ae60"><i class="fa-solid fa-leaf"></i></span></strong>')
-  .pauseFor(1000)
-  .deleteChars(10)
+  .pauseFor(300)
+  .deleteChars(9)
   .typeString('<strong><span style="color:#005b87">My</span><span style="color:#dd8a00">SQL</span></strong> <span style="color:#abaaab"><i class="fa-solid fa-database"> </i></span>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(10)
   .typeString('<strong><span style="color:#4181ed">S</span><span style="color:#ea4235">E</span><span style="color:#fabc05">O</span> <span><i class="fa-solid fa-magnifying-glass"></i></span></strong>')
-  .pauseFor(1000)
+  .pauseFor(300)
   .deleteChars(7)
   .start()
+
